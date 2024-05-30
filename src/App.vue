e735a82b03c520246828ec7448b47338017aeac2
Lucaalex00 <lu.al.cirio00@gmail.com>
2024-05-30 15:14:57 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+</script>
+
+
+
+<template>
+  <header>Header</header>
+  <h1>hello world</h1>
+<footer>Footer</footer>
+</template>
+
+<style scoped>
+template{
+  background-color: black;
+}
+h1,header,footer{
+  color: red;
+}
+</style>
