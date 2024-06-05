3ab22addbcb55402dda09147d2f539f5be4fedc9
Lucaalex00 <lu.al.cirio00@gmail.com>
2024-06-05 11:58:40 +0200

    add form in AppContacts Commit

--- src/views/AppContacts.vue
+++ src/views/AppContacts.vue
@@ -1,7 +1,32 @@
 
 <script>
     export default {
-        name: 'AppContacts',
+    name: 'AppContacts',
+        data(){
+            return {
+                name: '',
+                email: '',
+                message: '',
+                loading: true,
+                base_api_url: 'http://127.0.0.1:8000',
+                base_posts_url: '/api/contacts',
+            }
+    },
+    methods: {
+        sendMessage() {
+            const data = {
+                name: this.name,
+                email: this.email,
+                message:this.message,
+            }
+                const apiUrl=`${this.base_api_url}${this.base_posts_url}`
+            axios.post(apiUrl,data).then(response => {
+                    console.log(response)
+            }).catch(err =>{
+                  console.log(err)  
+                })
+            },
+        }
     }
 </script>
 
@@ -32,6 +57,31 @@
         <!-- /.row -->
     </div>
     <!-- /.container -->
+    <br>
+    <h2>Send an email</h2>
+    <br>
+    <form action="#" @submit.prevent=sendMessage()>
+        <div class="mb-3">
+            <label for="" class="form-label">Name</label>
+            <input type="text" class="form-control" name="name" id="name" aria-describedby="nameHelpId"
+                placeholder="name" v-model="name" />
+        </div>
+        <br>
+        <div class="mb-3">
+            <label for="" class="form-label">Email</label>
+            <input type="email" class="form-control" name="email" id="email" aria-describedby="emailHelpId"
+                placeholder="Random@mail.com" v-model="email" />
+        </div>
+        <br>
+        <div class="mb-3">
+            <label for="" class="form-label">Message</label>
+            <input type="text" class="form-control" name="message" id="message" aria-describedby="messageHelpId"
+                placeholder="message" v-model="message" />
+        </div>
+            <br>
+        <button type="submit">CONFIRM</button>
+    </form>
+
 </template>
 
 
